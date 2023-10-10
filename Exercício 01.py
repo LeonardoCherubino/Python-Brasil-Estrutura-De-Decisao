0b64a1c68682094c8dbe0a4ebfa70713eeb7f25c
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-10 20:58:16 -0300

    Python Brasil - Estrutura De Decisão
    Python Brasil - Estrutura De Decisão.
    Exercício 01

---
+++ Exercício 01.py
@@ -0,0 +1,11 @@
+## Faça um Programa que peça dois números e imprima o maior deles.
+
+Num1 = int(input('Entre com um número. '))
+Num2 = int(input('Entre com mais um número. '))
+
+if Num1 > Num2:
+    print('O primeiro número é maior. ')
+if Num2 > Num1:
+    print('O segundo número é maior. ')
+if Num1 == Num2:
+    print('Os números são iguais. ')
