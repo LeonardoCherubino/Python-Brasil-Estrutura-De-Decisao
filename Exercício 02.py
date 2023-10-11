7f2b24c899781420b7fdedbfc564dc941b302341
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-10 21:43:25 -0300

    Python Brasil - Estrutura De Decisão
    Python Brasil - Estrutura De Decisão

---
+++ Exercício 02.py
@@ -0,0 +1,9 @@
+## Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+Num1 = int(input('Entre com um número. '))
+if Num1 > 0:
+    print('O número é maior que zero. ')
+if Num1 < 0:
+    print('O número é menor que zero. ')
+if Num1 == 0:
+    print('Você digitou zero. ')
