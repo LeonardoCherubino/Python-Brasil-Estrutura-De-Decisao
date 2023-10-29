1a12a755c6be0eeb698ad81a9b466381a5f2e3ab
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-29 16:53:58 -0300

    Exercício 04
    Exercício 04

---
+++ Exercício 04.py
@@ -0,0 +1,20 @@
+## Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+Letra = input('Entre com uma letra. ')
+
+if Letra == 'a':
+    print('Você digitou uma vogal. ')
+elif Letra == 'e':
+    print('Você digitou uma vogal. ')
+elif Letra == 'i':
+    print('Você digitou uma vogal. ')
+elif Letra == 'o':
+    print('Você digitou uma vogal. ')
+elif Letra == 'u':
+    print('Você digitou uma vogal. ')
+else:
+    print('Você digitou uma consoante. ')
+
+
+    
+    
